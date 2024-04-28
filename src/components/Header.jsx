90973ffb841f71d7bced38a0d64acb812f65d33d
unknown <sharjeelahmed3808@gmail.com>
2024-04-28 15:06:14 -0700

    header changed for all devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import GuestDropDown from "./GuestDropDown";
 import UserDropDown from "./UserDropDown";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import useCheckAuth, { CheckAuthProvider, checkAuth } from "../Hooks/checkAuth";
-import MobileHeader from "./MobileHeader";
 import Catagorybar from "./Catagorybar";
 import { NavLink, useLocation } from "react-router-dom";
 import { ScissorsLineDashedIcon } from "lucide-react";
@@ -22,53 +21,45 @@ function Header() {
 
   return (
     <>
-      {/* header for mobile devices */}
-      {/* <MobileHeader /> */}
-
-      {/* header for large devices */}
       <div className="z-50  sticky bg-white top-0 border-b-2 md:pb-2 md:block">
         <div className=" px-1 md:px-10 h-16 flex items-center justify-between">
           <div className="xl:mr-40">
             <NavLink to={"/"} className="flex items-center gap-1">
-              <img className=" w-8 h-8 " src={logo} />
+              <img className="ml-2 w-8 h-8 " src={logo} />
               <span className="text-[#FF385C] font-bold text-lg hidden md:block">
                 airbnb
               </span>
             </NavLink>
           </div>
-          <div className="hidden  lg:flex w-96  justify-around ">
-            <span className="rounded-full cursor-pointer px-4 py-2 hover:bg-gray-100 hover:text-black">
-              Stays
-            </span>
-            <span className="rounded-full cursor-pointer text-gray-400 px-4 py-2 hover:bg-gray-100 hover:text-black">
-              Experiences
-            </span>
-            <span className="rounded-full cursor-pointer text-gray-400  px-4 py-2 hover:bg-gray-100 hover:text-black">
-              Online Experiences
-            </span>
-          </div>
-          <div className="flex items-center gap-3">
-            <NavLink to={"yourhome"}>
-              <span className="rounded-full px-3 text-md py-2 hover:bg-gray-100 hover:text-black">
-                Airbnb your home
-              </span>
-            </NavLink>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              aria-hidden="true"
-              role="presentation"
-              focusable="false"
-              className="hidden md:block w-4 h-6 mr-2"
+          <div className="hidden  lg:flex gap-3 w-96  ">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive ? "text-black" : "text-gray-400"
+                } px-5 cursor-pointer text-md py-2  hover:bg-gray-100 hover:text-black rounded-full`
+              }
             >
-              <path d="M8 .25a7.77 7.77 0 0 1 7.75 7.78 7.75 7.75 0 0 1-7.52 7.72h-.25A7.75 7.75 0 0 1 .25 8.24v-.25A7.75 7.75 0 0 1 8 .25zm1.95 8.5h-3.9c.15 2.9 1.17 5.34 1.88 5.5H8c.68 0 1.72-2.37 1.93-5.23zm4.26 0h-2.76c-.09 1.96-.53 3.78-1.18 5.08A6.26 6.26 0 0 0 14.17 9zm-9.67 0H1.8a6.26 6.26 0 0 0 3.94 5.08 12.59 12.59 0 0 1-1.16-4.7l-.03-.38zm1.2-6.58-.12.05a6.26 6.26 0 0 0-3.83 5.03h2.75c.09-1.83.48-3.54 1.06-4.81zm2.25-.42c-.7 0-1.78 2.51-1.94 5.5h3.9c-.15-2.9-1.18-5.34-1.89-5.5h-.07zm2.28.43.03.05a12.95 12.95 0 0 1 1.15 5.02h2.75a6.28 6.28 0 0 0-3.93-5.07z" />
-            </svg>
+              <span>Stays</span>
+            </NavLink>
+
+            <NavLink
+              to={"/yourhome"}
+              className={({ isActive }) =>
+                `${
+                  isActive ? "text-black" : "text-gray-400"
+                } px-3 cursor-pointer text-md py-2    hover:bg-gray-100 hover:text-black rounded-full`
+              }
+            >
+              Airbnb your home
+            </NavLink>
+          </div>
+          <div className="flex items-center gap-3 shadow-md rounded-full">
             {user === true ? <UserDropDown /> : <GuestDropDown />}
           </div>
         </div>
         {Head && (
           <div>
-            <HeaderSeachBar />
             <Catagorybar />
           </div>
         )}
